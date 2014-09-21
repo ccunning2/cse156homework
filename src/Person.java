3e2d76e5d427d22d917bcd5b55f07c4ff600e0ec
ccunning2 <cameron.cunning@huskers.unl.edu>
2014-09-21 18:08:50 -0500

    Many changes. Customer no longer inherits person. Methods to write XML and read both customers and persons data files are written and working. Address class created and utilized.
    Many of these changes will need to be reflected in the design document.
    
    Still need to: 
    Write code to handle products.
    Make code self executable.
    Clean up and tweak to get desired results.

--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,40 @@
 
 public class Person {
-	private String code; //Alpha-numeric designation from old system
-	private String name; //Person's name in last, first format
-	private String address; //“STREET,CITY,STATE,ZIP,COUNTRY” mailing address of customer
-	private String[] eAddress; //E-mail addresses. Array, arraylist, or String variable
+	private String personCode; //Alpha-numeric designation from old system
+	private String firstName;
+	private String lastName;
+	private Address address;
+	private String[] emails;
 	
+	public Person(String personCode, String name, Address address, String email) {
+		this.personCode = personCode;
+		String[] names = name.split(",");
+		this.lastName = names[0];
+		this.firstName = names[1];
+		this.address = address;
+		this.emails = email.split(",");
+		
+	}
 
+	public String getPersonCode() {
+		return personCode;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public String[] getEmails() {
+		return emails;
+	}
+	
+	
 }
